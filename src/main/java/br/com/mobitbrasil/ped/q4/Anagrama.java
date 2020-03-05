08f6faabcd60c893f2ba4bf299687ff1bbfe6d75
deyvisson01 <deyvisson_souza01@hotmail.com>
2020-03-05 19:17:19 -0300

    Quarta questão.
    Desenvolvimento básico da quarta tarefa.

--- src/main/java/br/com/mobitbrasil/ped/q4/Anagrama.java
+++ src/main/java/br/com/mobitbrasil/ped/q4/Anagrama.java
@@ -1,5 +1,6 @@
 package br.com.mobitbrasil.ped.q4;
 
+import java.util.*;
 public class Anagrama {
 
     /**
@@ -10,8 +11,33 @@ public class Anagrama {
      *
      * @return
      */
-    public static long checarAnagramas(String a, String b){
-        throw new UnsupportedOperationException();
+    public static void recebeString(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com a primeira palavra desejada: ");
+        String palavra1 = sc.nextLine();
+        System.out.println("Entre com a segunda palavra desejada: ");
+        String palavra2 = sc.nextLine();
+        sc.close();
+
+        if(checarAnagramas(palavra1, palavra2)) System.out.println("É um anagrama! ");
+        else System.out.println("Não é um anagrama! ");
+
+    }
+    public static boolean checarAnagramas(String a, String b){
+        String palavra1 = a.replace(" ","").toLowerCase();
+        String palavra2 = b.replace(" ","").toLowerCase();
+        
+        char [] palavra1_array = palavra1.toCharArray();
+        char [] palavra2_array = palavra2.toCharArray();
+        
+        Arrays.sort(palavra1_array);
+        Arrays.sort(palavra2_array);
+
+        if (Arrays.equals(palavra1_array, palavra2_array)) return true;
+        else return false;
+
+        // throw new UnsupportedOperationException();
     }
 
 }
---
+++ src/main/java/br/com/mobitbrasil/ped/q4/Main.java
@@ -0,0 +1,10 @@
+package br.com.mobitbrasil.ped.q4;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        
+        Anagrama.recebeString();
+
+    }
+}
