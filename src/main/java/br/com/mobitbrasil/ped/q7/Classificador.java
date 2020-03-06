51c51caa230c833564f0c64cc8ed4910a5ebe787
deyvisson01 <deyvisson_souza01@hotmail.com>
2020-03-05 22:45:59 -0300

    Sétima questão.
    Desenvolvimento básico da sétima tarefa. (Incompleto)

--- src/main/java/br/com/mobitbrasil/ped/q7/Classificador.java
+++ src/main/java/br/com/mobitbrasil/ped/q7/Classificador.java
@@ -1,5 +1,8 @@
 package br.com.mobitbrasil.ped.q7;
 
+import java.io.*;
+import java.util.*;
+
 public class Classificador {
 
     /**
@@ -12,7 +15,40 @@ public class Classificador {
      * @return
      */
     public static long contarPalavras(String palavra){
-        throw new UnsupportedOperationException();
-    }
+        
+        List<String[]> lista = new ArrayList<>();   
+         
+        try {
+            String path = "C:/Users/deyvi/Documents/GitHub/estagiario-java/src/main/resources/palavras.txt";
+            File caminhoDoTexto = new File(path);
+            FileReader lerCaminhoDoTexto = new FileReader(caminhoDoTexto);
+            BufferedReader lerArquivo = new BufferedReader(lerCaminhoDoTexto);
 
+            String str;
+            
+            while(lerArquivo.ready()){
+                str = lerArquivo.readLine();
+                lista.add(str.split(" "));
+            } 
+            lerArquivo.close();
+
+        } catch(IOException e) {
+            System.out.println("Arquivo não encontrado!");
+        } finally {
+            //System.out.println(lista);
+        }
+
+        long count = 0;
+        int i = 0;
+        Iterator<String[]> iterator = lista.iterator();
+        while (iterator.hasNext()) {
+          System.out.printf("Posição %d- %s\n", i, iterator.next());
+          if (iterator.next().equals(palavra)) count++;
+          i++;
+        }
+        
+        return count;
+
+        //throw new UnsupportedOperationException();
+    }
 }
---
+++ src/main/java/br/com/mobitbrasil/ped/q7/Main.java
@@ -0,0 +1,10 @@
+package br.com.mobitbrasil.ped.q7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       System.out.println("A quantidade é: " + Classificador.contarPalavras("conta")) ;
+
+    }
+}
