e7536d349c4283e6b3c9afb510b42a7c382eb644
deyvisson01 <deyvisson_souza01@hotmail.com>
2020-03-07 02:32:55 -0300

    Segunda questão.
    Revisão de código.

--- src/main/java/br/com/mobitbrasil/ped/q2/Palindromo.java
+++ src/main/java/br/com/mobitbrasil/ped/q2/Palindromo.java
@@ -14,19 +14,19 @@ public class Palindromo {
     public static void recebePalindromo(){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Entre com o palíndromo: ");
+        System.out.println("Entre com o palíndromo: "); // Entrada da palavra a ser testada.
         String palavra = sc.nextLine();
         sc.close();
         
-        if(checarPalindromo(palavra)) System.out.println("É um palíndromo! ");
+        if(checarPalindromo(palavra)) System.out.println("É um palíndromo! "); // Chamada do método que irá realziar a verificação; Teste com o retorno do método.
         else System.out.println("Não é um palíndromo!");
     }
 
     public static boolean checarPalindromo( String texto) {
-        String palavra = texto.replace(" ","").toLowerCase();
-        String palindromo = new StringBuilder(palavra).reverse().toString();
+        String palavra = texto.replace(" ","").toLowerCase(); // Eliminando os espaços e mantendo o texto recebido em letras minúsculas.
+        String palindromo = new StringBuilder(palavra).reverse().toString(); // Revertendo a entrada.
 
-        if (palavra.equals(palindromo)) return true;
+        if (palavra.equals(palindromo)) return true; // Teste de validade da expressão.
         else return false;
 
         // throw new UnsupportedOperationException();
