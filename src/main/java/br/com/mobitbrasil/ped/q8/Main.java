b074f9e64095f51e3ef1f71b39d0e4c70f1b3b3c
deyvisson01 <deyvisson_souza01@hotmail.com>
2020-03-07 03:03:53 -0300

    Oitava questão.
    Revisão de código.

--- src/main/java/br/com/mobitbrasil/ped/q8/Main.java
+++ src/main/java/br/com/mobitbrasil/ped/q8/Main.java
@@ -3,7 +3,7 @@ package br.com.mobitbrasil.ped.q8;
 public class Main {
 
     public static void main(String[] args) {
-        Ordenar.formaLista();
+        Ordenar.formaLista(); // Chamada do método que irá receber a lista de pessoas.
 
     }
 }
--- src/main/java/br/com/mobitbrasil/ped/q8/Ordenar.java
+++ src/main/java/br/com/mobitbrasil/ped/q8/Ordenar.java
@@ -16,25 +16,25 @@ public class Ordenar{
      */
 
     public static void formaLista(){
-        List<Pessoa> listaPessoa = new ArrayList<Pessoa>();
+        List<Pessoa> listaPessoa = new ArrayList<Pessoa>(); // Inicia a lista de Pessoas.
 
         try {
-            String path = "src/main/resources/pessoas.txt";
+            String path = "src/main/resources/pessoas.txt"; // Caminho do arquivo que contém os dados das pessoas.
             File caminhoDoTexto = new File(path);
             FileReader lerCaminhoDoTexto = new FileReader(caminhoDoTexto);
-            BufferedReader lerArquivo = new BufferedReader(lerCaminhoDoTexto);
-            String curtLine = lerArquivo.readLine();
+            BufferedReader lerArquivo = new BufferedReader(lerCaminhoDoTexto); // Abertura do arquivo.
+            String curtLine = lerArquivo.readLine(); // Leitura do arquivo.
 
-            while (curtLine != null) {
-                String a[] = curtLine.split(";");
+            while (curtLine != null) { // Executa enquanto contem caracteres na linha.
+                String a[] = curtLine.split(";"); // Separa os dados pelo ";".
                 int a1 = Integer.parseInt(a[1]);
-                Pessoa pessoa = new Pessoa(a[0], a1, a[2], a[3]);
-                listaPessoa.add(pessoa);
+                Pessoa pessoa = new Pessoa(a[0], a1, a[2], a[3]); // Inicializa um objeto Pessoa com os dados do arquivo.
+                listaPessoa.add(pessoa); // Adiciona na lista de Pessoas.
 
-                curtLine = lerArquivo.readLine();
+                curtLine = lerArquivo.readLine(); // Leitura da próxima linha do arquivo.
             }
             lerArquivo.close();
-            ordenarPessoas(listaPessoa);
+            ordenarPessoas(listaPessoa); // Chamada do método ordenador passando a lista de Pessoas como parâmetro.
 
         } catch ( Exception e) {
             // TODO: handle exception
@@ -43,8 +43,8 @@ public class Ordenar{
 
     public static List<Pessoa> ordenarPessoas(List<Pessoa> pessoas){
         System.out.println("Ordenando pessoas pela idade:");
-        Collections.sort(pessoas, (Pessoa pessoa1, Pessoa pessoa2) -> pessoa1.getIdade().compareTo(pessoa2.getIdade()));
-        pessoas.forEach(p -> System.out.println(p.getNome()+"\t"+p.getIdade()));
+        Collections.sort(pessoas, (Pessoa pessoa1, Pessoa pessoa2) -> pessoa1.getIdade().compareTo(pessoa2.getIdade())); // Uso de Collections para ordenar a lista usando como teste o atributo idade de cada objeto Pessoa.
+        pessoas.forEach(p -> System.out.println(p.getNome()+"\t"+p.getIdade())); // Print da lista ordenada.
         return pessoas;
     }
 }
