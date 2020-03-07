c77eb24849865b3b58dfb98bb98ebb68bb4527d4
deyvisson01 <deyvisson_souza01@hotmail.com>
2020-03-07 10:35:19 -0300

    Teste unitário questão quatro.

---
+++ src/main/java/br/com/mobitbrasil/ped/q4/AnagramaTest.java
@@ -0,0 +1,21 @@
+package br.com.mobitbrasil.ped.q4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AnagramaTest {
+
+	@Test
+	void test() {
+		String entradaTest_1 = "pato pato";
+		String entradaTest_2 = "topa topa";
+		
+		String errorTest_1 = "bola bola";
+		String errorTest_2 = "topa topa";
+		
+		assertTrue(Anagrama.checarAnagramas(entradaTest_1, entradaTest_2));
+		assertFalse(Anagrama.checarAnagramas(errorTest_1, errorTest_2));
+	}
+
+}
