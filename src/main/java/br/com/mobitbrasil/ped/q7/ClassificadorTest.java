d94e6aa75212ec3430d8cb9222d5822f38eb7463
deyvisson01 <deyvisson_souza01@hotmail.com>
2020-03-07 10:51:27 -0300

    Teste unitário questão 7.

---
+++ src/main/java/br/com/mobitbrasil/ped/q7/ClassificadorTest.java
@@ -0,0 +1,25 @@
+package br.com.mobitbrasil.ped.q7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClassificadorTest {
+
+	@Test
+	void test() {
+		String entradaTest = "comerciais";
+		String entradaTest_2 = "o";
+		String entradaTest_3 = "palavra";
+		
+		long saidaTest = 34;
+		long saidaTest_2 = 721;
+		long saidaTest_3 = 1;
+		
+		assertEquals(saidaTest, Classificador.contarPalavras(entradaTest));
+		assertEquals(saidaTest_2, Classificador.contarPalavras(entradaTest_2));
+		assertNotEquals(saidaTest_3, Classificador.contarPalavras(entradaTest_3));
+		
+	}
+
+}
