f9e7961a145b5bcb430d92f9464062b2279313ee
deyvisson01 <deyvisson_souza01@hotmail.com>
2020-03-07 11:01:50 -0300

    Teste unitário questão oito.

---
+++ src/main/java/br/com/mobitbrasil/ped/q8/OrdenarTest.java
@@ -0,0 +1,38 @@
+package br.com.mobitbrasil.ped.q8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class OrdenarTest {
+
+	@Test
+	void test() {
+		List<Pessoa> listaPessoa = new ArrayList<Pessoa>();
+		List<Pessoa> listaPessoa_saida = new ArrayList<Pessoa>();
+		Pessoa pessoa_1 = new Pessoa("teste_1", 55, "teste email", "teste telefone");
+		Pessoa pessoa_2 = new Pessoa("teste_2", 44, "teste email", "teste telefone");
+		Pessoa pessoa_3 = new Pessoa("teste_3", 33, "teste email", "teste telefone");
+		Pessoa pessoa_4 = new Pessoa("teste_4", 22, "teste email", "teste telefone");
+		Pessoa pessoa_5 = new Pessoa("teste_5", 11, "teste email", "teste telefone");
+		
+		listaPessoa.add(pessoa_1);
+		listaPessoa.add(pessoa_2);
+		listaPessoa.add(pessoa_3);
+		listaPessoa.add(pessoa_4);
+		listaPessoa.add(pessoa_5);
+		
+		listaPessoa_saida.add(pessoa_5);
+		listaPessoa_saida.add(pessoa_4);
+		listaPessoa_saida.add(pessoa_3);
+		listaPessoa_saida.add(pessoa_2);
+		listaPessoa_saida.add(pessoa_1);
+		
+		assertEquals(listaPessoa_saida, Ordenar.ordenarPessoas(listaPessoa));
+		
+	}
+
+}
